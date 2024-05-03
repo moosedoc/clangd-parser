244cfae81b60b161a20a7e5abffe3983c60a584e
Spencer Graf <graf.spencer@gmail.com>
2024-05-03 08:27:46 -0700

    updates. trim null terminator from strings since rust is safe enough to not use them

--- src/lib.rs
+++ src/lib.rs
@@ -18,25 +18,25 @@ pub fn run(p: PathBuf) -> clangd::ClangdDatabase {
 async fn _run(p: PathBuf) -> clangd::ClangdDatabase {
     let mut to_file: clangd::ClangdFileMap = HashMap::new();
     let mut to_id: clangd::ClangdIdMap = HashMap::new();
-    let mut p = p.join(".cache");
-    if !p.exists() {
+    let mut path = p.join(".cache");
+    if !path.exists() {
         panic!("Unable to find .cache!");
     }
-    p = p.join("clangd").join("index");
-    if !p.exists() {
+    path = path.join("clangd").join("index");
+    if !path.exists() {
         panic!("Has clangd been run?");
     }
 
-    let rd = fs::read_dir(p.as_path()).unwrap();
+    let rd = fs::read_dir(path.as_path()).unwrap();
     for entry in rd {
         let e = entry.unwrap();
         if e.file_type().unwrap().is_file() {
             let _ret = clangd::ClangdFile::parse(e.path()).await;
             if _ret.is_ok() {
-                let db = _ret.unwrap();
                 let mut fname = e.file_name().to_str().unwrap().to_string();
                 let parts: Vec<&str> = fname.split('.').collect();
                 fname = format!("{}.{}", parts[0], parts[1]);
+                let db = _ret.unwrap();
                 let _ = to_file.entry(fname).or_insert(db.clone());
                 for sym in db.symbols.data.iter() {
                     let _ = to_id.entry(sym.id).or_insert(sym.clone());
